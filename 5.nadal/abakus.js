67a2dd2f1c9bd4f48e58f4906702da11843a1eed
SmirnoffAce <bbrokan@itcollege.ee>
2017-03-19 14:18:59 +0200

    Step 3

--- 5.nadal/abakus.js
+++ 5.nadal/abakus.js
@@ -1,9 +1,17 @@
 window.onload = function(){
+	function changePosition(){
+		if(this.style.cssFloat == 'left')
+			this.style.cssFloat = 'right';
+		else
+			this.style.cssFloat = 'left';	
+	}
 	var a = document.getElementsByClassName('bead');
 	for(var i = 0; i < a.length; i++){
 		if(a[i].className == "bead right")
 			a[i].style.cssFloat = 'left';
 		else
 			a[i].style.cssFloat = 'right';
+		
+		a[i].onclick = changePosition;	
 	}
 }
