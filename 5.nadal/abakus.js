4ebd9f152d34f7d435de7651d3f8cc09182f8549
SmirnoffAce <bbrokan@itcollege.ee>
2017-03-19 14:18:46 +0200

    Step 2

--- 5.nadal/abakus.js
+++ 5.nadal/abakus.js
@@ -1,3 +1,9 @@
 window.onload = function(){
-
+	var a = document.getElementsByClassName('bead');
+	for(var i = 0; i < a.length; i++){
+		if(a[i].className == "bead right")
+			a[i].style.cssFloat = 'left';
+		else
+			a[i].style.cssFloat = 'right';
+	}
 }
